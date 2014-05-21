14f19fd204cb63e2f4e03360ca59f65d0470c956
balazs.zsoldos <balazs.zsoldos@everit.biz>
2014-05-21 03:10:40 +0200

    Commiting empty project structure

---
+++ core/src/main/java/org/everit/osgi/servicecollector/ServiceCollector.java
@@ -0,0 +1,11 @@
+package org.everit.osgi.servicecollector;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class ServiceCollector {
+
+    public ServiceCollector(Map<String, Collection<String>> servicesClausesByName) {
+        // TODO Auto-generated constructor stub
+    }
+}
